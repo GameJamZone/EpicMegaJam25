1c528841a57acfdbbf67c688a8283c4adb4252b2
Gabriel <gabrielmenezes0297@gmail.com>
2025-10-21 22:29:21 +0100

    upgrades

--- Source/HereWeGoAgain/Data/SpawnTableRow.h
+++ Source/HereWeGoAgain/Data/SpawnTableRow.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GameplayEffect.h"
 #include "GameplayTagContainer.h"
 #include "Engine/DataTable.h"
 #include "SpawnTableRow.generated.h"
@@ -20,4 +21,29 @@ struct FSpawnTableRow : public FTableRowBase
 	// Optional spawn offset
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta=(ToolTip="Random spawn offset radius."))
 	float SpawnRadius = 100.f;
+};
+
+USTRUCT(BlueprintType)
+struct FUpgradeTableRow : public FGameplayTagTableRow
+{
+	GENERATED_BODY()
+
+	UPROPERTY(editAnywhere, BlueprintReadWrite, Category="Upgrade", meta=(ToolTip="Name of the upgrade to spawn."))
+	FText UpgradeName;
+
+	UPROPERTY(editAnywhere, BlueprintReadWrite, Category="Upgrade", meta=(ToolTip="Description of the upgrade to spawn."))
+	FText Description;
+
+	UPROPERTY(editAnywhere, BlueprintReadWrite, Category="Upgrade", meta=(ToolTip="Icon to display for the upgrade."))
+	UTexture2D* Icon;
+
+	UPROPERTY(editAnywhere, BlueprintReadWrite, Category="Upgrade", meta=(ToolTip="Gameplay effect to apply when the upgrade is purchased."))
+	TSoftClassPtr<UGameplayEffect> UpgradeEffect;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Upgrade", meta=(ToolTip="Gameplay tag to apply to the actor when the upgrade is purchased."))
+	FGameplayTag SetByCallerTag;
+
+	// Default magnitude to apply
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Upgrade", meta=(ToolTip="Default magnitude to apply to the upgrade when purchased."))
+	float Magnitude = 0.0f;
 };
